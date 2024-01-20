988f40707546f02f5130541e2af45e3513f7078f
Marcio Curvello <marciocurvello@gmail.com>
2024-01-20 09:47:11 -0300

    add search bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import SearchBar from './components/SearchBar'
 import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
-    <Sidebar />
+    <div>
+      <Sidebar />
+      <SearchBar />
+    </div>
   )
 }
 
---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,8 @@
+import "./styles.css";
+// import Search from './assets/search.svg';
+
+export default function SearchBar() {
+  return (
+    <input type="text" placeholder="Digite o que você procura" className="search-bar" />
+  );
+}
